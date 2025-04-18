539a36f9736d927b44f881851dde59158fd28696
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2025-04-18 22:19:10 +0200

    fix api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getTrendingMovies } from "../api";
+import { getTrendingMovies } from "../api.js";
 import MovieList from "../components/MovieList";
 
 const HomePage = () => {
